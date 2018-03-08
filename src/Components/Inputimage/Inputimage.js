a789a87595b2619909e4e5640e910e9f22c90ee8
Ashish <ashishcodes4@gmail.com>
2018-03-09 03:50:52 +0530

    resolving image tag issue

--- src/Components/Inputimage/Inputimage.js
+++ src/Components/Inputimage/Inputimage.js
@@ -6,7 +6,8 @@ const Inputimage = ({ imageurl, box }) => {
 	return(
 			<div className='center ma'>
 				<div className='absolute mt2'>
-					{imageurl && <img id='inputimage' alt='inputimg' src={imageurl} width='500px' height='auto' />}
+					{/* <img id='inputimage' alt='inputimg' src={imageurl} width='500px' height='auto' /> */}
+					{ imageurl? <img id='imputimage' alt='inputimg' src={imageurl} width='500px' height='auto' /> : null }
 					<div className='bounding-box' style={{top: box.topRow, bottom: box.bottomRow, right: box.rightCol, left: box.leftCol}} ></div>
 				</div>
 			</div>
