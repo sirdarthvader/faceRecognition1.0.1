f6da6f1d4997253a9aae3efbac7e2934a61a0cc6
Ashish Singh <ashishcodes4@gmail.com>
2018-03-27 15:01:32 -0400

    adding new navigation file

--- src/Components/Navigation/Navigation.js
+++ src/Components/Navigation/Navigation.js
@@ -1,13 +1,22 @@
 import React from 'react';
+import Tilt from 'react-tilt';
+import brain from './brain.png';
+import './Navigation.css';
 
-const Navigation = ({onRouteChange}) => {
-	return (
-		<nav style={{ display: 'flex', justifyContent: 'space-between' }}>
-			<p className='f3 pa3'>Smart Brain</p>
-			<p onClick={() => onRouteChange('signin')} className="pointer">Sign Out</p>
-		</nav>
-		);
+const Navigation = () => {
+  return (
+    <div className="nav pa3">
+      <div>
+        <Tilt className="Tilt" options={{ max: 25 }} style={{ height: 150, width: 150 }} >
+          <div className="Tilt-inner">
+            <img src={brain} alt='brain' />
+          </div>
+        </Tilt>
+      </div>
+      <div className="login">
+        <span className='f3 link dim black underline pa3 pointer '>Signout</span>
+      </div>
+    </div>
+  );
 }
-
-
 export default Navigation;
