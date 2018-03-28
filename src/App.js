e6930fd693d44d84392221bc929f580b296340bd
Ashish Nandan Singh <ashishcodes4@gmail.com>
2018-03-28 06:44:21 +0530

    done

--- src/App.js
+++ src/App.js
@@ -108,13 +108,14 @@ class App extends Component {
 
 
   render() {
+    const { input, imgurl, box, isSignedIn, route}
     return (
       <div className="App">
         <Particles className='particles'
           params={particlesOption}
         />
-        <Navigation isSignedIn={this.state.isSignedIn} onRouteChange={this.onRouteChange} />
-        { this.state.route === 'home' ?
+        <Navigation isSignedIn={isSignedIn} onRouteChange={this.onRouteChange} />
+        { route === 'home' ?
         <div>
           <Logo />
             <ImageLinkForm
@@ -123,12 +124,12 @@ class App extends Component {
               refreshPage={this.refreshPage}
             />
           <Inputimage
-            box={this.state.box}
-            imageurl={this.state.input}
+            box={box}
+            imageurl={input}
           />
         </div> 
         : (
-          this.state.route === 'signin'
+          route === 'signin'
           ? <Signin  onRouteChange={this.onRouteChange} />
           : <Register onRouteChange={this.onRouteChange} />
         )
