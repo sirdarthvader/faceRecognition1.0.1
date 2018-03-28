24fd3e2d06889da8fcbec22838d6e3dce5539fbc
Ashish Nandan Singh <ashishcodes4@gmail.com>
2018-03-28 05:54:00 +0530

    adding register route

--- src/Components/Register/Register.js
+++ src/Components/Register/Register.js
@@ -15,15 +15,18 @@ const Register = ({onRouteChange}) => {
 				      </div>
 				      <div className="mt3">
 				        <label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
-				        <input className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="email" name="email-address"  id="email-address" />
+				        <input className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
+								 type="email" name="email-address"  id="email-address" />
 				      </div>
 				      <div className="mv3">
 				        <label className="db fw6 lh-copy f6" htmlFor="password">Password</label>
-				        <input className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="password" name="password"  id="password" />
+				        <input className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" 
+								type="password" name="password"  id="password" />
 				      </div>				      
 				    </fieldset>
 				    <div className="">
-				      <input onClick={() => onRouteChange('home')} className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib" type="submit" value="Sign in" />
+				      <input onClick={() => onRouteChange('home')} className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib"
+							 type="submit" value="Sign in" />
 				    </div>
 				    <div className="lh-copy mt3">
 				      <a href="#0" className="f6 link dim black db">Register</a>
